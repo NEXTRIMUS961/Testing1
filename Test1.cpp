011ac7586c5631b5ff52df597485a8ef0e6a8979
Maxim <nextrimus121@yandex.ru>
2024-09-16 22:05:57 +0300

    init

---
+++ Test1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    std::cout << "What is your name?";
+    std::cin >> name;
+    std::cout << "Hello, " << name << "!";
+
+    //std::cout << "Привет!";
+    //std::cin.get();
+
+    //cout << "Hello" << "Dad!";
+    //std::cout << "Hello, world!" << std::endl;
+
+    return 0;
+}
